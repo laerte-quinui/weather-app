2a14f94f6901da5abe74f47a9ad00b06ac029421
Laerte Quinui <laertequinuio@gmail.com>
2026-09-15 15:54:56 -0300

    ✨feat: add type definitions for measurement units

---
+++ types/units.ts
@@ -0,0 +1,4 @@
+export type GlobalUnit = "metric" | "imperial";
+export type TemperatureUnit = "celsius" | "fahrenheit";
+export type WindSpeedUnit = "km/h" | "mph";
+export type PrecipitationUnit = "mm" | "in";
