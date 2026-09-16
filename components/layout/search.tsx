2dd30e7cb6aa856d997978982003dd0a5f081438
Laerte Quinui <laertequinuio@gmail.com>
2026-09-16 20:22:52 -0300

    ✨feat: enhance search component with Combobox for improved input handling and loading states

--- components/layout/search.tsx
+++ components/layout/search.tsx
@@ -1,41 +1,68 @@
 "use client";
 
 import { useGetLocation } from "@/api/location";
+import { Location } from "@/api/location/types";
 import { debounce } from "@/utils";
-import { Search02Icon } from "@hugeicons/core-free-icons";
+import { LoaderIcon, Search02Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Button, Field, InputGroupAddon } from "../ui";
 import {
-  Button,
-  Field,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupInput,
-} from "../ui";
+  Combobox,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxList,
+  ComboboxStatus,
+} from "../ui/combobox";
 
 const Search = () => {
+  const [openSuggestions, setOpenSuggestions] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<string>("");
   const [searchTerm, setSearchTerm] = useState<string>("");
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   // Debounce the search term update to avoid excessive API calls
   const debouncedSetSearchTerm = useMemo(() => {
-    return debounce((value: string) => {
-      setSearchTerm(value);
-    }, 300);
+    return debounce((val: string) => {
+      setSearchTerm(val);
+    }, 500);
   }, []);
 
-  const { data: locations } = useGetLocation(searchTerm, {
-    enabled: !!searchTerm, // Only fetch when search term is not empty
-  });
+  const handleInputValueChange = (val: string) => {
+    setInputValue(val);
+    setOpenSuggestions(val.length > 0);
+    debouncedSetSearchTerm(val);
+  };
+
+  const locationDetails = (location: Location) => {
+    return [
+      location.admin1,
+      location.admin2,
+      location.admin3,
+      location.admin4,
+      location.country,
+    ]
+      .filter(Boolean)
+      .join(", ");
+  };
 
   // Cancel any pending debounced calls on unmount
   useEffect(() => {
-    return () => {
-      debouncedSetSearchTerm.cancel();
-    };
+    return () => debouncedSetSearchTerm.cancel();
   }, [debouncedSetSearchTerm]);
 
-  console.log(locations);
+  const { data: locations, isLoading: isLoadingLocations } = useGetLocation(
+    searchTerm,
+    {
+      enabled: !!searchTerm, // Only fetch when search term is not empty
+    },
+  );
+
+  const isDebouncePending = inputValue !== searchTerm && inputValue.length > 0;
+  const isLoading = isDebouncePending || isLoadingLocations;
 
   return (
     <Field
@@ -43,24 +70,65 @@ const Search = () => {
       className="mt-16 max-w-xl gap-4 self-center"
     >
       {/* Search input */}
-      <InputGroup>
-        <InputGroupInput
+      <Combobox
+        items={locations?.results}
+        open={openSuggestions}
+        onOpenChange={setOpenSuggestions}
+        onInputValueChange={handleInputValueChange}
+        itemToStringValue={(location: Location) => location.name}
+        itemToStringLabel={(location: Location) => location.name}
+        openOnInputClick={false}
+      >
+        <ComboboxInput
+          ref={inputRef}
           size="lg"
+          showClear
+          showTrigger={false}
+          className="w-full"
           placeholder="Search for a place..."
-          value={inputValue}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-            debouncedSetSearchTerm(e.target.value);
-          }}
-        />
-        <InputGroupAddon>
-          <HugeiconsIcon
-            icon={Search02Icon}
-            strokeWidth={2}
-            className="size-4"
-          />
-        </InputGroupAddon>
-      </InputGroup>
+        >
+          <InputGroupAddon>
+            <HugeiconsIcon
+              icon={Search02Icon}
+              strokeWidth={2}
+              className="size-4"
+            />
+          </InputGroupAddon>
+        </ComboboxInput>
+
+        {/* Menu content */}
+        <ComboboxContent sideOffset={12} anchor={inputRef}>
+          {/* Statuses */}
+          <ComboboxStatus isVisible={isLoading}>
+            <HugeiconsIcon
+              icon={LoaderIcon}
+              strokeWidth={2}
+              className="size-4 animate-spin"
+            />
+            Search in progress
+          </ComboboxStatus>
+
+          {!isLoading && (
+            <ComboboxEmpty>No search results found.</ComboboxEmpty>
+          )}
+
+          {/* List of search results */}
+          <ComboboxList>
+            {(location: Location) => (
+              <ComboboxItem
+                key={location.id}
+                value={location}
+                className="flex flex-col items-start gap-1 text-start"
+              >
+                <p>{location.name}</p>
+                <p className="text-xs/relaxed text-neutral-300">
+                  {locationDetails(location)}
+                </p>
+              </ComboboxItem>
+            )}
+          </ComboboxList>
+        </ComboboxContent>
+      </Combobox>
 
       {/* Search button */}
       <Button size="lg">Search</Button>
