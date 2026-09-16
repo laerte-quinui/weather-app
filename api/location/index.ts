09d58fe08d0e53bce9ce32f73d698b7150f1d5fd
Laerte Quinui <laertequinuio@gmail.com>
2026-09-16 08:37:31 -0300

    ✨feat: add location fetching functionality with custom hook and types

---
+++ api/location/index.ts
@@ -0,0 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import { geocoding } from "../instances";
+import { LocationError, LocationResponse } from "./types";
+
+/**
+ * Fetches location data based on a search query.
+ * @param location - The name for the location.
+ * @returns A promise that resolves to an array of location results.
+ */
+const getLocation = (location?: string): Promise<LocationResponse> => {
+  const params = {
+    count: 10,
+    language: "en",
+    format: "json",
+  };
+
+  return geocoding
+    .get<LocationResponse>("/search", {
+      params: {
+        name: location,
+        params,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    });
+};
+
+/**
+ * Custom hook to fetch location data using React Query.
+ * @param location - The name for the location.
+ * @returns An object containing the query result, loading state, and error state.
+ */
+export const useGetLocation = (location?: string) =>
+  useQuery<LocationResponse, LocationError>({
+    queryKey: ["location", location],
+    queryFn: () => getLocation(location),
+  });
---
+++ api/location/types.ts
@@ -0,0 +1,32 @@
+export interface Location {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  elevation: number;
+  feature_code: string;
+  country_code: string;
+  admin1_id: number;
+  admin2_id: number;
+  admin3_id: number;
+  admin4_id: number;
+  timezone: string;
+  population: number;
+  postcodes: Array<string>;
+  country_id: number;
+  country: string;
+  admin1: string;
+  admin2: string;
+  admin3: string;
+  admin4: string;
+}
+
+export interface LocationResponse {
+  results: Array<Location>;
+  generationtime_ms: number;
+}
+
+export interface LocationError {
+  error: boolean;
+  reason: string;
+}
