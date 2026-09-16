5e3c8a66c709515d9f18ae8b383d8e962037fc39
Laerte Quinui <laertequinuio@gmail.com>
2026-09-16 20:22:45 -0300

    ✨feat: add Combobox component with various subcomponents and input handling

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -3,14 +3,16 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "cn";
 import * as React from "react";
 
+const inputSizeVariants = {
+  default: "h-8.5 px-3 py-0.5 text-sm md:text-xs/relaxed!",
+  lg: "h-12 rounded-lg px-4 py-1 text-base/relaxed!",
+};
+
 const inputVariants = cva(
   "w-full min-w-0 cursor-pointer rounded-md border border-transparent bg-neutral-800 bg-clip-padding text-sm transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-xs/relaxed file:font-medium file:text-foreground placeholder:text-neutral-300 hover:bg-neutral-700 focus-visible:border-background focus-visible:ring-2 focus-visible:ring-neutral-0 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-2 md:text-xs/relaxed",
   {
     variants: {
-      size: {
-        default: "h-8.5 px-3 py-0.5 text-sm md:text-xs/relaxed!",
-        lg: "h-12 rounded-lg px-4 py-1 text-base/relaxed!",
-      },
+      size: inputSizeVariants,
     },
     defaultVariants: {
       size: "default",
@@ -35,4 +37,4 @@ function Input({
   );
 }
 
-export { Input, inputVariants };
+export { Input, inputSizeVariants, inputVariants };
