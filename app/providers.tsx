e8ed6d51d6da6b155ede1654965da55de3d43f46
Laerte Quinui <laertequinuio@gmail.com>
2026-09-22 19:50:00 -0300

    ✨feat: add TooltipProvider and related components for enhanced UI tooltips

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { TooltipProvider } from "@/components/ui";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getQueryClient } from "./get-query-client";
@@ -8,7 +9,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   const queryClient = getQueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <TooltipProvider>{children}</TooltipProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
