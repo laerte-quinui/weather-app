6c77f42b7c35fcbf05eddf36947d0e729195bb9f
Laerte Quinui <laertequinuio@gmail.com>
2026-09-22 19:01:35 -0300

    ✨feat: integrate current coordinates and forecast data into TodayCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { getForecast } from "@/api/forecast";
+import { getCurrentCoordinates } from "@/api/location";
 import {
   DailyForecast,
   Header,
@@ -6,8 +8,27 @@ import {
   TodayCard,
   TodayDetails,
 } from "@/components/layout";
+import { getQueryClient } from "./get-query-client";
 
-export default function Home() {
+export default async function Home() {
+  const currCoord = await getCurrentCoordinates();
+
+  const queryClient = getQueryClient();
+  await queryClient.query({
+    queryKey: [
+      "forecast",
+      {
+        lat: currCoord.latitude,
+        lon: currCoord.longitude,
+      },
+    ],
+    queryFn: () =>
+      getForecast({
+        latitude: currCoord.latitude,
+        longitude: currCoord.longitude,
+      }),
+  });
+
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
       <main className="container flex w-full flex-1 flex-col p-4 md:py-8">
@@ -23,7 +44,7 @@ export default function Home() {
         <div className="mt-10 grid h-full flex-1 grid-cols-1 gap-6 md:grid-cols-6">
           {/* Left Column */}
           <section className="col-span-1 flex h-full flex-col gap-6 md:col-span-4">
-            <TodayCard />
+            <TodayCard coord={currCoord} />
             <TodayDetails />
             <DailyForecast />
           </section>
--- components/layout/today-card.tsx
+++ components/layout/today-card.tsx
@@ -1,27 +1,47 @@
 "use client";
 
+import { useGetForecast } from "@/api/forecast";
+import { useGetCurrentLocation } from "@/api/location";
 import bgLarge from "@/public/images/bg-today-large.svg";
 import bgSmall from "@/public/images/bg-today-small.svg";
-import Sunny from "@/public/images/icon-sunny.webp";
+import { Coordinates } from "@/types";
+import { formatTemperature, formatWeatherCode, getCurrentDate } from "@/utils";
+import { HugeiconsIcon } from "@hugeicons/react";
 import Image from "next/image";
 
-const TodayCard = () => {
+const TodayCard = ({ coord }: { coord: Coordinates }) => {
+  const { data: userLocation } = useGetCurrentLocation();
+  const { data: forecast } = useGetForecast(coord, {
+    select: (d) => d.current,
+  });
+
+  const weatherCode = formatWeatherCode({
+    code: Number(forecast?.weather_code) || 0,
+    isDaytime: forecast?.is_day === 1,
+  });
+
   return (
     <div className="relative flex h-full min-h-fit w-full flex-[fit-content] flex-col items-center justify-between gap-4 overflow-hidden rounded-xl bg-blue-500 p-6 md:flex-1 md:flex-row">
       {/* Location */}
       <div className="z-1 flex h-fit flex-col gap-2 text-center md:text-start">
-        <p className="text-2xl font-semibold">City, Country</p>
-        <p className="font-light text-neutral-200">Week day, Month day, Year</p>
+        <p className="text-2xl font-semibold">
+          {userLocation?.cityName}, {userLocation?.countryName}
+        </p>
+        <p className="font-light text-neutral-200">{getCurrentDate()}</p>
       </div>
 
       {/* Weather */}
-      <div className="z-1 flex h-fit items-center gap-2">
-        <Image
-          src={Sunny}
-          alt="Sunny weather icon"
-          className="size-22 md:size-32"
-        />
-        <p className="text-6xl font-semibold italic lg:text-8xl">0º</p>
+      <div className="z-1 flex h-fit items-center gap-4">
+        <div className="flex items-center justify-center rounded-2xl bg-neutral-0 p-2">
+          <HugeiconsIcon
+            icon={weatherCode.icon}
+            className="size-12 text-neutral-900 md:size-14"
+          />
+        </div>
+
+        <p className="text-6xl font-semibold italic lg:text-8xl">
+          {formatTemperature(forecast?.temperature_2m)}
+        </p>
       </div>
 
       {/* Background */}
